a588288c3a2a79a49a5f3fabfbb08df90c0bdfa0
Hemanth <hemanthvasireddy2003@gmail.com>
2024-04-16 23:56:18 +0530

    added covariances

--- gps_odometry/gps_to_odom.py
+++ gps_odometry/gps_to_odom.py
@@ -5,94 +5,47 @@ from rclpy.node import Node
 from sensor_msgs.msg import NavSatFix
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose
-from geometry_msgs.msg import Quaternion
-from sensor_msgs.msg import Imu
-from geometry_msgs.msg import Vector3Stamped
+from geometry_msgs.msg import QuaternionStamped
+
 import math
 import utm
+import numpy as np
 
 
 class GPS_TO_ODOM(Node):
     def __init__(self):
         super().__init__('gps_to_odom')
 
-        self.gnss_subscriber=self.create_subscription(Vector3Stamped,'/filter/positionlla',self.gnss_callback,10)
-        self.imu_subscriber=self.create_subscription(Imu,'/imu/data',self.imu_callback,10)
+        self.gnss_subscriber=self.create_subscription(NavSatFix,'/gnss',self.gnss_callback,10)
+        self.imu_subscriber=self.create_subscription(QuaternionStamped,'/filter/quaternion',self.imu_callback,10)
 
         self.odom_publisher=self.create_publisher(Odometry,'/odometry/gps',10)
-        timer_period=0.25
-        self.timer = self.create_timer(timer_period, self.odom_callback) 
+
+        self.rotation=None
+
+        
+        self.initial_yaw=0.0
 
         self.i=0
         self.j=0
 
-        self.wait_for_fix=0
-        self.wait_for_fix_time=250
-
-        self.gnss=None
-        self.utm=None
-
-        self.temp_yaw=None
-        self.initial_yaw=None
-        self.yaw=None
-        self.orientation=None
-        self.utm=None
         self.initial_utm=None
 
     def gnss_callback(self,gnss):
-        latitude=gnss.vector.x
-        longitude=gnss.vector.y
+        latitude=gnss.latitude
+        longitude=gnss.longitude
 
         if self.j==0:
-            self.temp_utm=utm.from_latlon(latitude,longitude)
+            self.initial_utm=utm.from_latlon(latitude,longitude)
             self.j=1
 
-        self.initial_utm=self.temp_utm
+        
         self.utm=utm.from_latlon(latitude,longitude)
 
-    def imu_callback(self,imu):
-        orientation=imu.orientation
-        w=orientation.w
-        x=orientation.x
-        y=orientation.y
-        z=orientation.z
-        siny_cosp = 2 * (w * z + x * y)
-        cosy_cosp = w * w + x * x - y * y - z * z
-
-        if self.i==0:
-            self.temp_yaw=math.atan2(siny_cosp, cosy_cosp)
-            self.i=1
-
-        self.initial_yaw=self.temp_yaw
-        self.yaw=math.atan2(siny_cosp, cosy_cosp)
-        self.orientation=orientation
-        self.angular_velocity=round(imu.angular_velocity.z,4)
-
-    def pose(self):
-
-        self.initial_x=(self.initial_utm[0]*math.cos(self.initial_yaw))-(self.initial_utm[1]*math.sin(self.initial_yaw))
-        self.initial_y=(self.initial_utm[0]*math.sin(self.initial_yaw))+(self.initial_utm[1]*math.cos(self.initial_yaw))
-
-        self.temp_x=(self.utm[0]*math.cos(self.initial_yaw))-(self.utm[1]*math.sin(self.initial_yaw))
-        self.temp_y=(self.utm[0]*math.sin(self.initial_yaw))+(self.utm[1]*math.cos(self.initial_yaw))
-
-        self.x=self.temp_x-self.initial_x
-        self.y=self.temp_y-self.initial_y
-        coordinates=[self.x,self.y]
-        return coordinates
-
-    def odom_callback(self):
         odom=Odometry()
         pose=Pose()
-        print("initial yaw=", self.initial_yaw)
-        print("yaw=", self.yaw)
 
-        print(self.wait_for_fix)
-        if self.wait_for_fix<(self.wait_for_fix_time*4):
-            self.wait_for_fix+=1
-
-
-        elif self.initial_utm!=None:
+        if(gnss.position_covariance[0]<1.0 and gnss.position_covariance[4]<1.0 and self.i==1):
             coordinates=self.pose()
             x=coordinates[0]
             y=coordinates[1]
@@ -103,9 +56,37 @@ class GPS_TO_ODOM(Node):
             pose.position.y=y
             odom.pose.pose=pose
 
+            odom.pose.covariance[0]=(self.rotation @ np.array([[gnss.position_covariance[0],0.0],[0.0,gnss.position_covariance[4]]]) @ self.rotation.T)[0,0]
+            odom.pose.covariance[7]=(self.rotation @ np.array([[gnss.position_covariance[0],0.0],[0.0,gnss.position_covariance[4]]]) @ self.rotation.T)[1,1]
+
+
             odom.child_frame_id="imu_link"
             odom.header.frame_id="odom"
             self.odom_publisher.publish(odom)
+
+    def imu_callback(self,orientation):
+        w=orientation.quaternion.w
+        x=orientation.quaternion.x
+        y=orientation.quaternion.y
+        z=orientation.quaternion.z
+        siny_cosp = 2 * (w * z + x * y)
+        cosy_cosp = w * w + x * x - y * y - z * z
+
+        if self.i==0:
+            self.initial_yaw=math.atan2(siny_cosp, cosy_cosp)
+            self.rotation=np.array([[np.cos(self.initial_yaw), -np.sin(self.initial_yaw)], [np.sin(self.initial_yaw), np.cos(self.initial_yaw)]])
+            self.i=1
+
+    def pose(self):
+        initial_coordinates = self.rotation @ np.array([[self.initial_utm[0]],[self.initial_utm[1]]])
+
+        temp_coordinates = self.rotation @ np.array([[self.utm[0]],[self.utm[1]]])
+
+        x = (temp_coordinates-initial_coordinates)[0,0]
+        y = (temp_coordinates-initial_coordinates)[1,0]
+        coordinates = [x,y]
+        return coordinates
+
 
 
 def main(args=None):
